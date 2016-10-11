674e525fdd0dd02446094ddee82d424718638e93
Cristian Flores <thecris.danny@gmail.com>
2016-10-11 00:21:13 -0600

    Primera prueba de autenticacion

--- APITX/APITX/settings.py
+++ APITX/APITX/settings.py
@@ -51,6 +51,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'PAGE_SIZE': 10
 }
