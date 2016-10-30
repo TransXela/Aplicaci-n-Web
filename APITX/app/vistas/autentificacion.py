9c7b746682112e64d2c9a71d197bf6403c5b7431
Cristian Flores <thecris.danny@gmail.com>
2016-10-30 13:33:25 -0600

    correccion
    se arreglo el problema de que cuando se obtenia datos del token solo
    devolvia duenio y no pmt o cultura

--- APITX/app/vistas/autentificacion.py
+++ APITX/app/vistas/autentificacion.py
@@ -3,26 +3,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
-from app.serializables import UserSerializer
-from app import permisos
 from rest_framework.response import Response
-#from rest_framework.decorators import api_view, authentication_classes, permission_classes
-
-
 from rest_framework.authtoken.models import Token
-
-"""
-
-@api_view(['GET'])
-#@authentication_classes((SessionAuthentication, BasicAuthentication,))
-#@permission_classes((IsAuthenticated,))
-def example_view(request, user, pass, format=None):
-    content = {
-        'user': unicode(user),  # `django.contrib.auth.User` instance.
-        'auth': unicode(request.auth),  # None
-    }
-    return Response(content)
-"""
+from app.models import TxdDuenio, TxdPmt, TxcCultura
+from app.serializables import UserSerializer, TxdPmtS, TxdDuenioS, TxcCulturaS
 
 @api_view(['POST'])
 def token(request, format=None):
@@ -36,10 +20,15 @@ def token(request, format=None):
             return Response("susuario y contasenia incorrecta", tatus=status.HTTP_404_NOT_FOUND)
 
         if objUsuario.check_password(request.data.get('pass')):
+
             datos = {}
-            serUsuario = UserSerializer(objUsuario).data
+            if TxdPmt.objects.filter(usuario=objUsuario.pk).exists():
+                datos['usuario'] = TxdPmtS(TxdPmt.objects.get(usuario=objUsuario.pk)).data
+            elif TxdDuenio.objects.filter(usuario=objUsuario.pk).exists():
+                datos['usuario'] = TxdDuenioS(TxdDuenio.objects.get(usuario=objUsuario.pk)).data
+            elif TxcCultura.objects.filter(usuario=objUsuario.pk).exists():
+                datos['usuario'] = TxcCulturaS(TxcCultura.objects.get(usuario=objUsuario.pk)).data
             token, created = Token.objects.get_or_create(user=objUsuario)
-            datos['usuario'] = serUsuario
             datos['token'] = token.key
             return Response(datos)
         else:
